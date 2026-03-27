1418a1e54b497f729e57b6a15e6ec38f29f0f60c
Shuja94 <shujau.hameed@gmail.com>
2026-03-28 00:17:22 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/yookie-s-safe-stream/'
+})
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
